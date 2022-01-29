5e47a0bf03294fbc094d64d020f48434cf36ce19
odzen <jsebastian.va@gmail.com>
2022-01-29 00:08:42 -0500

    Solution 1.1

--- Course 1 - Introduction to Python/Course_code/ProblemSet_1/Problem1/problem1.py
+++ Course 1 - Introduction to Python/Course_code/ProblemSet_1/Problem1/problem1.py
@@ -6,3 +6,11 @@ Created on Fri Jan 28 23:55:41 2022
 @author: juan
 """
 
+s= 'azcbobobegghakl'
+vowels=0
+
+for letter in s:
+    if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u' :
+        vowels+=1
+
+print("Number of vowels: " + str(vowels))
