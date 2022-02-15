c122096049c3a95fb6424361eb80f95c1d8f97d7
odzen <jsebastian.va@gmail.com>
2022-02-15 01:18:48 -0500

    Organizando

---
+++ Course 1 - Introduction to Python/Course_code/Unit2/ProblemSet_2/problem1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 23:55:41 2022
+
+@author: juan velasquez
+
+Course: Introduction to Computer Science and Programming Using Python - Edx
+"""
+
