e200b9798ea5a3b85a7b96e4daab5470a567f850
odzen <jsebastian.va@gmail.com>
2022-02-15 20:30:09 -0500

    Problem 2.3

---
+++ Course 1 - Introduction to Python/Unit2/ProblemSet_2/Problem3/problem3.py
@@ -0,0 +1,13 @@
+initBalance = balance
+monthlyInterestRate = annualInterestRate/12.0
+low = balance/12.0
+high = (balance * ((1.0 + monthlyInterestRate)**12))/12.0
+epsilon = 0.01
+minPay = (high + low)/2.0
+month = 0
+def calculate(month, balance, minPay, monthlyInterestRate):
+    while month <12:
+        unpaidBalance = balance - minPay
+        balance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
+        month += 1
+    return balance   
