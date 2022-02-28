e9c449585ca6fb1be03cb0afa1d605d276d44b0d
odzen <jsebastian.va@gmail.com>
2022-02-28 01:04:42 -0500

    Exercises unit 2

---
+++ Course 1 - Introduction to Python/Unit2/Exercises/fourth_power.py
@@ -0,0 +1,15 @@
+def square(x):
+    '''
+    takes in one number and returns the square of that number.
+    x: int or float.
+    '''
+    # Your code here
+    return x**2
+
+def fourthPower(x):
+    '''
+    takes in one number and returns that value raised to the fourth power.
+    x: int or float.
+    '''
+    return square(x)**2
+    
---
+++ Course 1 - Introduction to Python/Unit2/Exercises/odd.py
@@ -0,0 +1,10 @@
+def odd(x):
+    '''
+    takes in one number and returns True when the number is odd and False otherwise.
+
+    returns: True if x is odd, False otherwise
+    '''
+    if(x%2!=0):
+        return True
+    else:
+        return False
