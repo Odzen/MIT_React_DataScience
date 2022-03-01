83da74169d45c3b52cf8bcdbc31878c452408a61
odzen <jsebastian.va@gmail.com>
2022-03-01 15:36:58 -0500

    4th problem midterm

--- Course 1 - Introduction to Python/Midterm_exam/Problem6.py
+++ Course 1 - Introduction to Python/Midterm_exam/Problem6.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  1 15:29:37 2022
+def gcd(a, b):
+    """
+    Calculates the greatest common divisor of two positive integers. 
+    The gcd of two or more integers, when at least one of them is not zero, 
+    is the largest positive integer that divides the numbers without a remainder.
+    a, b: two positive integers
+    Returns the greatest common divisor of a and b
+    """
+    
+    if(b==0):
+        return a
+    else:
+        return gcd(b, a % b)
 
-@author: juan
-"""
 
+print(gcd(20,12))
