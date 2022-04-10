e1b4899492962bceb87a011428843c864fb9a250
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-04-09 22:10:00 -0500

    Exercise 2, graphs implementation

--- Course 2 - Introduction to Computational Thinking and Data Science/Unit1-Dynamic Programming/Lecture 3 - Graph Problems/graph.py
+++ Course 2 - Introduction to Computational Thinking and Data Science/Unit1-Dynamic Programming/Lecture 3 - Graph Problems/graph.py
@@ -1,5 +1,4 @@
 # Class Graph, Edge, Node
-import Digraph
 
 class Node(object):
     def __init__(self, name):
@@ -23,9 +22,63 @@ class Edge(object):
         return self.src.getName()+'->'  + self.dest.getName()
     
 
+class Digraph(object):
+    """"edges is a dict mapping each node to a list of its children"""
+    def __init__(self):
+        self.edges={}
+        
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError('Duplicate Node')
+        else:
+            self.edges[node] = []
+    
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+        
+    def childrenOf(self, node):
+        return self.edges[node]
+    
+    def hasNode(self, node):
+        return node in self.edges
+    
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
 
-class graph (Digraph):
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:result = result + src.getName() + '->'+ dest.getName() + '\n'
+        return result[:-1] #omit final newline
+
+
+class Graph (Digraph):
     def addEdge(self, edge):
         Digraph.addEdge(self, edge)
         rev = Edge(edge.getDestination(), edge.getSource())
         Digraph.addEdge(self, rev)
+
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
+        g.addNode(Node(name))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
+    
+#print(buildCityGraph(Graph))
